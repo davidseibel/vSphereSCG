2cddfa18df9d818d5ab36a57db84b055ca548aaf
Uhrig <Ross.Uhrig@state.sd.us>
2017-08-28 23:24:42 -0500

    changes to resolve

--- Functions/Resolve-vSCGCompliance.ps1
+++ Functions/Resolve-vSCGCompliance.ps1
@@ -18,32 +18,18 @@ function Resolve-vSCGCompliance {
             Date Modified  : 08/25/2017
     #>
 
-    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
-    $path = "$(Split-Path $Invocation.MyCommand.Path)\SCG_6.5\SCG_6.5.xml"
-    $xml = [xml](Get-Content $path)
-    
-    foreach ($guideline in $xml.vSCG.Guideline) {
-        if ($guideline.FunctionSet -ne "unsupported") {
-            # Call $guideline.FunctionGet
-        } else {
-            Write-Warning "Guideline '$($guideline.Name)' cannot be retrieved with this function!"
-        }
+    [CmdletBinding()]
+    param(
+      [Parameter(Position = 0, ValueFromPipeline = $true)]
+      $InputObject
+    )
+
+    BEGIN{
+        $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+        $path = "$(Split-Path $Invocation.MyCommand.Path)\SCG_6.5\SCG_6.5.xml"
+        $xml = [xml](Get-Content $path)
     }
 
-    # Loop through compliance objects
-
-      # if not compliant
-        # Read XML file for Set function
-
-        # if Set function exists
-          # Call set function
-        # else (if set function does not exist)
-          # Throw warning that guideline cannot be remediated
-        # end if
-
-      # end if
-
-    # end loop
     PROCESS {
         foreach ($object in $InputObject) {
             foreach ($guideline in $xml.vSCG.Guideline) {
@@ -52,9 +38,12 @@ function Resolve-vSCGCompliance {
                     Invoke-Expression ('$object | ' + $guideline.FunctionSet)
 
                 } else {
-                    Write-Warning "Guideline '$($guideline.Name)' cannot be retrieved with this function!"
+                    Write-Warning "Guideline '$($guideline.Name)' cannot be resolved with this function!"
                 }
             }
         }
+    }
+    END{
+
     }
 }
